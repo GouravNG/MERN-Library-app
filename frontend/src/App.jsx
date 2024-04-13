345f45b8135d5bc3c8b88a953d9a6d2deab4ee7f
Gourav <gouravgunaga555@gmail.com>
2024-04-13 17:16:06 +0530

    Added routing and modified server port
    added the router dom dependency
    added the basic routing for the reference
    Modified the viter server port

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-    <h1>Library App</h1>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/register' element={<h1>Register</h1>}></Route>
+                <Route path='/login' element={<h1>Login</h1>}></Route>
+                <Route path='/' element={<h1>Home</h1>}></Route>
+                <Route path='*' element={<h1>404</h1>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
