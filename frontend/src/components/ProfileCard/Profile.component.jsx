a2bd4c8665d57fffacc368d0b38a519e1b701c7c
Gourav <gouravgunaga555@gmail.com>
2024-05-06 17:54:34 +0530

    Reactify the profile hover function

--- frontend/src/components/ProfileCard/Profile.component.jsx
+++ frontend/src/components/ProfileCard/Profile.component.jsx
@@ -1,21 +1,25 @@
+import { useAuthContext } from '../../context/auth.context'
+import { nameIcon } from '../../utils/nameIcon.mjs'
 import './profile.css'
 export const Profilecard = () => {
+    const { authUser } = useAuthContext()
+    const { firstname, lastname, credit, booksOwned, booksRented } = JSON.parse(JSON.stringify(authUser))
     return (
         <>
             <div className='profile-top'>
                 <div className='profie-top-left'>
-                    <p>Gourav Gunaga</p>
-                    <p>Credit:10</p>
+                    <p>{`${firstname} ${lastname}`}</p>
+                    <p>Credit:{credit}</p>
                 </div>
-                <div className='profile-top-right'>TT</div>
+                <div className='profile-top-right'>{nameIcon(firstname, lastname)}</div>
             </div>
             <div className='profile-botton'>
                 <div className='books-info'>
                     <div className='booksrelated'>
-                        <p>Books owned:2</p>
+                        <p>Books owned:{booksOwned}</p>
                     </div>
                     <div className='booksrelated'>
-                        <p>Books borrowed: 4</p>
+                        <p>Books borrowed: {booksRented}</p>
                     </div>
                 </div>
                 <div className='other-buttons'>
